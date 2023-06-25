e5a686e471978208cdcdbbd760ea04988d583e39
Jellybean Melt <jjoonwoo.park@gmail.com>
2023-06-25 19:53:15 -0400

    multification table

--- main.py
+++ main.py
@@ -237,36 +237,45 @@ print(results)
 
 
 #5-8
-from requests import get
-from bs4 import BeautifulSoup
+# from requests import get
+# from bs4 import BeautifulSoup
 
-og_url = "https://weworkremotely.com/remote-jobs/search?utf8=&term="
-searched_term = "python"
+# og_url = "https://weworkremotely.com/remote-jobs/search?utf8=&term="
+# searched_term = "python"
 
-response = get(f"{og_url}{searched_term}")
-if response.status_code != 200:
-  print("Can't request website")
-else:
-  results = []
-  soup = BeautifulSoup(response.text, "html.parser")
-  jobs = soup.find_all('section', class_="jobs")
-  for job_section in jobs:
-    job_posts = job_section.find_all('li')
-    job_posts.pop(-1)
-    for post in job_posts:
-      anchors = post.find_all('a')
-      anchor = anchors[1]
-      link = anchor['href']
-      company, kind, region = anchor.find_all('span', class_='company')
+# response = get(f"{og_url}{searched_term}")
+# if response.status_code != 200:
+#   print("Can't request website")
+# else:
+#   results = []
+#   soup = BeautifulSoup(response.text, "html.parser")
+#   jobs = soup.find_all('section', class_="jobs")
+#   for job_section in jobs:
+#     job_posts = job_section.find_all('li')
+#     job_posts.pop(-1)
+#     for post in job_posts:
+#       anchors = post.find_all('a')
+#       anchor = anchors[1]
+#       link = anchor['href']
+#       company, kind, region = anchor.find_all('span', class_='company')
 
-      title = anchor.find('span', class_='title')
-      job_data = {
-        'company': company.string,
-        'region': region.string,
-        'position': title.string
-      }
-      results.append(job_data)
-      #print(company.string, kind.string, region.string, title.string)
-for result in results:
-  print(result)
-  print("///////////////////")
+#       title = anchor.find('span', class_='title')
+#       job_data = {
+#         'company': company.string,
+#         'region': region.string,
+#         'position': title.string
+#       }
+#       results.append(job_data)
+#       #print(company.string, kind.string, region.string, title.string)
+# for result in results:
+#   print(result)
+#   print("///////////////////")
+
+for nums in range(2, 10):
+  n = 1
+  while n < 10:
+    print(f"{nums} x {n} = {nums*n}") 
+    n = n+1
+    if n == 10:
+      print()
+      
