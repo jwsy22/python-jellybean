69df30f39f9fae63da19525f3d5e7a15c1e16e7f
Jellybean Melt <jjoonwoo.park@gmail.com>
2023-07-02 14:47:18 -0400

    5.14

--- main.py
+++ main.py
@@ -354,34 +354,45 @@ print(results)
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
+from extractors.wwr import extract_wwr_jobs
+from requests import get
 
 options = Options()
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 options.add_experimental_option("detach", True)  # 브라우저 꺼짐 방지 코드
 browser = webdriver.Chrome(options=options)
-browser.get(
-    'https://kr.indeed.com/jobs?q=python')
+base_url = "https://kr.indeed.com/jobs?q="
+search_term = "python"
+# response = get(f"{base_url}{search_term}")
 
+
+browser.get(
+    f"{base_url}{search_term}")
+
+# if response.status_code != 200:
+#      print("Cannot request page")
+# else:
+results = []
 soup = BeautifulSoup(browser.page_source, "html.parser")
 job_list = soup.find("ul", class_="jobsearch-ResultsList")
-
 jobs = job_list.find_all('li', recursive=False)
-
+resutls = []
 for job in jobs:
     zone = job.find("div", class_="mosaic-zone")
     if zone == None:
-    #     print("job li")
-    # else:
-    #     print("mosaic li")
       anchor = job.select_one("h2 a")
       title = anchor['aria-label']
       link = anchor['href']
       company = job.find("span", class_="companyName")
       location = job.find("div", class_="companyLocation")
       job_data = {
-         'link' : f"https://kr.indeed.com{link}",
+         'link': f"https://kr.indeed.com{link}",
          'company': company.string,
          'location': location.string,
          'position': title
       }
+
+      resutls.append(job_data)
+
+print(resutls)
